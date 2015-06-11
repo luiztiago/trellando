203e828cf0c52b42297334c0738fe6df71037526
Rodolfo Santos <rodolfoasantos@gmail.com>
2015-06-11 16:08:32 -0300

    Update trello conf

--- lib/trello.rb
+++ lib/trello.rb
@@ -2,7 +2,7 @@ Trello.module_eval do
   def self.set_credentials(token)
     Trello.configure do |config|
       config.member_token = token
-      config.developer_public_key = ENV['DEVELOPER_PUBLIC_KEY']
+      config.developer_public_key = ENV['TRELLO_PUBLIC_KEY']
     end
   end
 end
--- lib/trello/session.rb
+++
@@ -1,8 +0,0 @@
-module Trello
-  class Session < Struct.new(:token)
-
-    def start
-      Trello.set_credentials(token)
-    end
-  end
-end
