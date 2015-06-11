72caebb43dffff239bcaa4c1c26a6a35c074d7d3
Rodolfo Santos <rodolfoasantos@gmail.com>
2015-06-11 14:28:42 -0300

    Add ruby-trello configuration

---
+++ config/initializers/trello.rb
@@ -0,0 +1,6 @@
+Trello.configure do |config|
+  config.consumer_key = ENV['TRELLO_CONSUMER_KEY']
+  config.consumer_secret = ENV['TRELLO_CONSUMER_SECRET']
+  config.oauth_token = ENV['TRELLO_OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['TRELLO_OAUTH_TOKEN_SECRET']
+end
