7518f957519104590acb1ceac3b3bf2e0893657f
Rodolfo Santos <rodolfoasantos@gmail.com>
2015-06-12 02:55:56 -0300

    Show active users

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,4 +7,8 @@ class Member < OpenStruct
   def cards
     @_cards ||= board_cards.select { |card| card.idMembers.include?(id) }
   end
+
+  def average_points
+    (cards.map(&:points).sum / cards.count.to_f).round(2)
+  end
 end
