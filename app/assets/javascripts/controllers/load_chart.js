0d1e02717c4a5e034167717fc9cb38c8135f0f25
luiztiago <luiztiago@gmail.com>
2015-06-12 04:05:16 -0300

    loaded charts

--- app/assets/javascripts/controllers/load_chart.js
+++ app/assets/javascripts/controllers/load_chart.js
@@ -1,20 +1,32 @@
 S.controller("load-chart", function(input, options) {
+  var defaults, ctx, myLineChart, data, labels, amounts;
 
-  var defaults, ctx, myLineChart, data;
+  labels = [];
+  amounts = [];
 
   defaults = {
     responsive: true
   };
 
+  $(input.data('values')).find('li').each(function(){
+    var label = $(this).data('label'),
+      amount = $(this).data('amount');
+
+    labels.push(label);
+    amounts.push(amount);
+  });
+
+  console.log(labels, amounts);
+
   data = {
-    labels: ["Luiz", "Diego", "Rodolfo"],
+    labels: labels,
     datasets: [
       {
         label: "My First dataset",
         fillColor: "rgba(151,187,205,0.2)",
         strokeColor: "rgba(151,187,205,1)",
         pointColor: "rgba(151,187,205,1)",
-        data: [80, 45, 54]
+        data: amounts
       }
     ]
   };
