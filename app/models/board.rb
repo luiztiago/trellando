a80df046f36ce9c45cc9c683f1f4bec6fe0ee780
Rodolfo Santos <rodolfoasantos@gmail.com>
2015-06-11 23:12:50 -0300

    Parse trello card

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,5 +2,14 @@ class Board < OpenStruct
 
   def initialize(params)
     super(params)
+    set_cards
+  end
+
+  def all_cards
+    @_cards ||= cards.map { |card| Card.new(card) }
+  end
+
+  def find_card(id)
+    all_cards.find { |card| card.id == id }
   end
 end
