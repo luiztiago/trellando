6b38fb446c87bbc9e4e862d3d0c52a9ab0b3453e
Diego Medeiros <diegoo.fm@gmail.com>
2015-06-12 01:21:20 -0300

    New metrics on board page

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,19 +8,37 @@ class Board < OpenStruct
     @_cards ||= self[:cards].map { |card| Card.new(card) }
   end
 
-  def cards_with_members
-    @_cards_with_members ||= cards.select { |card| card.idMembers.present? }
-  end
-
   def members
     @_members ||= self[:members].map { |member| Member.new(member) }
   end
 
+  def average_points_per_member
+    (total_points / members_with_cards.count.to_f).round(2)
+  end
+
   def average_cards_per_member
-    (cards.count / members.count.to_f).round(2)
+    (cards_with_members.count / members_with_cards.count.to_f).round(2)
   end
 
   def find_card(id)
     all_cards.find { |card| card.id == id }
+  end
+
+  private
+
+  def total_points
+    cards_with_members.map(&:points).sum
+  end
+
+  def cards_with_members
+    @_cards_with_members ||= cards.select { |card| card.idMembers.present? }
+  end
+
+  def members_with_cards
+    @_members_with_cards ||= members.select { |member| member_ids_with_cards.include?(member.id) }
+  end
+
+  def member_ids_with_cards
+    @_member_ids_with_cards ||= cards_with_members.flat_map(&:idMembers).uniq
   end
 end
