ab00449e27f03db4a07bd1883b05dcd0b8ef1631
Rodolfo Santos <rodolfoasantos@gmail.com>
2015-06-11 22:04:06 -0300

    Client refactor

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,19 +1,21 @@
 require 'trello'
 
 class Client
+
+  DEFAULT_OPTIONS = { filter: :open, fields: :name }
+
   def initialize(token)
     @trello_client = Trello::Client.new(
       developer_public_key: ENV['TRELLO_PUBLIC_KEY'],
       member_token: token
     )
-    @params = params
   end
 
   def boards(params = {})
-    trello_client.get("/members/me/boards", params.merge(filter: :open, fields: :name))
+    trello_client.get("/members/me/boards", params.merge(DEFAULT_OPTIONS))
   end
 
   private
 
-  attr_reader :trello_client, :params
+  attr_reader :trello_client
 end
