f5773dc88e528cdea9198750cb02139221ff45ca
Diego Medeiros <diegoo.fm@gmail.com>
2015-06-12 03:59:35 -0300

    Displaying total points for each member

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,7 +8,11 @@ class Member < OpenStruct
     @_cards ||= board_cards.select { |card| card.idMembers.include?(id) }
   end
 
+  def total_points
+    @_total_points ||= cards.map(&:points).sum
+  end
+
   def average_points
-    (cards.map(&:points).sum / cards.count.to_f).round(2)
+    (total_points / cards.count.to_f).round(2)
   end
 end
