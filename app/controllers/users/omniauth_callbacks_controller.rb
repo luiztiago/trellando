67dbd087d2f516abb78af2fafa05059ab9a21dca
Diego Medeiros <diegoo.fm@gmail.com>
2015-06-15 18:54:56 -0300

    Fixing callback when user deny access to trello account

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,10 +3,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
+      sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "Trello") if is_navigational_format?
     else
       redirect_to root_url
     end
+  end
+
+  private
+
+  def after_omniauth_failure_path_for(scope)
+    root_url
   end
 end
